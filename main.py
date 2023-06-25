aa2859db862698cf450450ba06c9a8aebb2ab90f
Smokey 8 <jacobramsdell316@gmail.com>
2023-06-24 19:22:37 -0700

    Assignment 2: Converting Temp

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ def main():
     Use the same variables: celsius fahrenheit 
     ##################################################
     """
-
+    celsius = int(input('Enter celsius temperature: '))
+    fahrenhiet = (9/5) * celsius + 32
+    print(f'Temperature in F \t  {fahrenhiet: .2f}')
     """
     ########################################
     # Do not delete the return statement
     ########################################
     """
-    return celsius, fahrenheit
+    return celsius, fahrenhiet
 
 
 if __name__ == '__main__':
